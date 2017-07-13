78622b76321f26e4428e49f630a5b36a68a60e82
LongND14 <longnd14@fsoft.com.vn>
2017-07-13 16:13:10 +0700

    Initial project

---
+++ src/abstracts/Animal.java
@@ -0,0 +1,10 @@
+package abstracts;
+
+/**
+ * Created by LongND14 on 7/13/2017.
+ */
+public abstract class Animal {
+    protected String name;
+    protected int age;
+    public abstract void print();
+}
